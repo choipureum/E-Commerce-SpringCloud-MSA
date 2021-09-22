6ff1c7ce56f70a8218d173d331f93c1346af3a8a
ChoiPuReum <pooreumsunny@gmail.com>
2021-09-22 23:38:05 +0900

    springCloud gateway router filter config -01
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,16 +1,23 @@
 package com.example.firstservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
-public class FirstServiceController
-{
+public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome()
     {
         return "Welcome to the First service";
+    }
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello World in First Service";
     }
 }
