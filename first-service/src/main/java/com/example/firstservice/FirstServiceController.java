563dba205dbd47f44428948039ac91c0e08702be
ChoiPuReum <pooreumsunny@gmail.com>
2021-09-23 23:48:15 +0900

    Add RoundRobin LoadBalacer func in Eureka
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @Slf4j
 @RequestMapping("/first-service")
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome()
     {
@@ -21,7 +32,10 @@ public class FirstServiceController {
         return "Hello World in First Service";
     }
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from First Service";
+    public String check(HttpServletRequest request){
+        log.info("Server port= {}", request.getServerPort());
+
+        return  String.format("Hi, there. This is a message from First Service on PORT %s"
+            , env.getProperty("local.server.port"));
     }
 }
