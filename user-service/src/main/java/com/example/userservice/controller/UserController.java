33ff950beba8b270003653253402614af9447ef4
ChoiPuReum <pooreumsunny@gmail.com>
2021-09-26 23:48:21 +0900

    Connect user-service MSA, Spring Cloud Gateway
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.ws.rs.POST;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
     private Environment env;
     private UserService userService;
@@ -32,7 +32,7 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status(){
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on PORT %s", env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
