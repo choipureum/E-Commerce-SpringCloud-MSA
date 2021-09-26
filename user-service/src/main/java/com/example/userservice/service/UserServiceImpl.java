0721a6e89ab83f0a0c9afd1913ad4e63f92b2519
ChoiPuReum <pooreumsunny@gmail.com>
2021-09-26 23:31:22 +0900

    Add Spring Security
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,14 +7,21 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
-    UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -23,7 +30,7 @@ public class UserServiceImpl implements UserService{
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); //완벽히 맞아야하는 level으로 설정
         UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPwd("encrypted_password");
+        userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd())); //pwd 암호화
 
         userRepository.save(userEntity);
         UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
