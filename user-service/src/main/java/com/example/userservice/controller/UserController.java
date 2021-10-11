f1d9a13eaf06e938e4bd8d5c2f01a1ac47471af7
ChoiPuReum <pooreumsunny@gmail.com>
2021-10-11 23:59:52 +0900

    Fix typo
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.POST;
 import java.util.ArrayList;
 import java.util.List;
 
